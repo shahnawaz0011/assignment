4f5658dbf5ae4f8ee58be116f792ca2a5b020869
joseph-benoy <josephbenoy@gmail.com>
2021-12-09 21:11:08 +0530

    added registrants component

--- src/Components/pages/courses/Negotiation/Registrants/Registrants.jsx
+++ src/Components/pages/courses/Negotiation/Registrants/Registrants.jsx
@@ -3,9 +3,79 @@ import './Registrants.css';
 const Registrants = ()=>{
 
     return(
-        <div className="registrants__innerWrap">
-            
-        </div>
+        <table>
+            <tr>
+                <th>
+                    Name
+                </th>
+                <th>
+                    Email-id
+                </th>
+                <th>
+                    Country
+                </th>
+                <th>
+                    Registration Date
+                </th>
+                <th>
+                    Attended
+                </th>
+            </tr>
+            <tr>
+                <td>
+                    <img src='profile.png' className='profile'/>
+                    Max Clark
+                </td>
+                <td>
+                    maxclark@gmail.com
+                </td>
+                <td>
+                    England
+                </td>
+                <td>
+                    May 7,2021
+                </td>
+                <td>
+                    3/3
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <img src='profile.png' className='profile'/>
+                    Max Clark
+                </td>
+                <td>
+                    maxclark@gmail.com
+                </td>
+                <td>
+                    England
+                </td>
+                <td>
+                    May 7,2021
+                </td>
+                <td>
+                    1/3
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <img src='profile.png' className='profile'/>
+                    Max Clark
+                </td>
+                <td>
+                    maxclark@gmail.com
+                </td>
+                <td>
+                    England
+                </td>
+                <td>
+                    May 7,2021
+                </td>
+                <td>
+                    2/3
+                </td>
+            </tr>
+        </table>
     );
 }
 
